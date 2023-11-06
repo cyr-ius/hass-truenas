b185f35e18befc9038c23f6c3e7e65fd821f3a5d
cyr-ius <cyr-ius@ipocus.net>
2023-11-06 12:41:44 +0100

    Add remove device

--- custom_components/truenas/__init__.py
+++ custom_components/truenas/__init__.py
@@ -35,3 +35,10 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
         hass.data[DOMAIN].pop(entry.entry_id)
     return unload_ok
+
+
+async def async_remove_config_entry_device(
+    hass: HomeAssistant, config_entry: ConfigEntry, device_entry: DeviceEntry
+) -> bool:
+    """Remove a config entry from a device."""
+    return True
