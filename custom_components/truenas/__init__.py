b0062283bb0d904fbd68dc49b097f6ac78fac518
cyr-ius <cyr-ius@ipocus.net>
2026-05-25 18:56:27 +0200

    refactor: remove future annotations and unused listener

--- custom_components/truenas/__init__.py
+++ custom_components/truenas/__init__.py
@@ -1,7 +1,5 @@
 """Truenas platform configuration."""
 
-from __future__ import annotations
-
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.device_registry import DeviceEntry
@@ -22,14 +20,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: TruenasConfigEntry) -> b
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     # await async_setup_services(hass, coordinator)
-    entry.async_on_unload(entry.add_update_listener(_async_update_listener))
 
     return True
-
-
-async def _async_update_listener(hass: HomeAssistant, entry: TruenasConfigEntry):
-    """Reload entry if change option."""
-    await hass.config_entries.async_reload(entry.entry_id)
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
